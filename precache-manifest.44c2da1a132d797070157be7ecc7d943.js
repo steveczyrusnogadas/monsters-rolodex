42c323c7d3799d3edca0ddfd41cd947c5428d982
Steve Czyrus Nogadas <steve.nogadas@appetiser.com.au>
2020-09-24 19:22:59 +0800

    Updates

---
+++ precache-manifest.44c2da1a132d797070157be7ecc7d943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a4d85c5d85779c73543d20f5d1d395",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2ed87dcdb1368013e8c9",
+    "url": "/monsters-rolodex/static/css/main.0fbf7290.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed87dcdb1368013e8c9",
+    "url": "/monsters-rolodex/static/js/main.56673f81.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
